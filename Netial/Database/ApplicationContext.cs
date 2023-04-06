51ca7181f8b3c5435b93fee5d014a8625b8674df
deltaDelete <samansy@yandex.ru>
2023-04-06 15:10:05 +0700

    initial commit

---
+++ Netial/Database/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Netial.Models;
+
+namespace Netial.Database;
+
+public class ApplicationContext : DbContext {
+    private readonly IConfiguration _configuration;
+    private string _connectionString = string.Empty;
+
+    public DbSet<User> Users { get; set; } = null;
+
+    public ApplicationContext(IConfiguration configuration) {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseMySql(_connectionString, new MySqlServerVersion(new Version(8,0,32)));
+    }
+}
---
+++ Netial/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Netial.Models;
+
+public class User {
+    [Column("user_id")]
+    public Guid Id { get; set; }
+    [Column("firstname")]
+    public string FirstName { get; set; }
+    [Column("lastname")]
+    public string LastName { get; set; }
+    [Column("birchdate")]
+    public DateTime BirthDate { get; set; }
+    [Column("creationdate")]
+    public DateTime CreationDate { get; set; }
+    [Column("rating")]
+    public int Rating { get; set; }
+}
