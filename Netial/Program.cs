90e70efb81b53bce1008e79fe65f5a744a1e0b53
deltaDelete <samansy@yandex.ru>
2023-04-08 20:59:53 +0700

    update(Program): добавлены авторизация, аутентификация, POST для авторизации

--- Netial/Program.cs
+++ Netial/Program.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Netial.Data;
 using Netial.Database;
+using Netial.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationContext>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options => options.LoginPath = "/login");
 
@@ -28,15 +33,47 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapGet("/testuser", (ApplicationContext db) => {
-    return db.Accounts.ToList();
+    return db.Users.ToList();
 });
 app.MapGet("/hash/{passwd}/{salt}", (HttpRequest request, [FromRoute] string passwd, [FromRoute] string salt) => {
     if (string.IsNullOrEmpty(passwd) || string.IsNullOrEmpty(salt)) {
         return "400";
     }
     return Netial.Helpers.Cryptography.Sha256Hash(passwd+salt); 
+});
+app.MapPost("/login", async (string? returnUrl, HttpContext context, ApplicationContext db) => {
+
+    var form = context.Request.Form;
+    if (!form.ContainsKey("email") || !form.ContainsKey("password")) {
+        return Results.BadRequest("Логин/пароль пусты");
+    }
+
+    string email = form["email"];
+    string password = form["password"];
+    
+    var emailNormalized = email.ToUpper();
+    var query = db.Users
+        .AsEnumerable()
+        .Where(u => u.EmailNormalized == emailNormalized
+                    && u.PasswordHash == Cryptography.Sha256Hash(password+u.PasswordSalt));
+
+    if (!query.Any()) {
+        return Results.Unauthorized();
+    }
+    
+    var claims = new List<Claim> {
+        new Claim(ClaimTypes.Name, query.ElementAt(0).FirstName),
+        new Claim(ClaimTypes.Email, query.ElementAt(0).Email)
+    };
+    // создаем объект ClaimsIdentity
+    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    // установка аутентификационных куки
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+    return Results.Redirect(returnUrl??"/");
 });
 
 app.MapBlazorHub();
